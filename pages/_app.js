e412713e16abf884b7a924563b6d7f46b3eb85dd
electro998 <101902227+electro998@users.noreply.github.com>
2022-03-18 22:05:05 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import '../styles/globals.css'
-import React, { Component, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -14,17 +13,6 @@ function MyApp({ Component, pageProps }) {
   )
     
 }
-
-  useEffect(() => {
-    window.location.href = "https://google.com/about";
-  }, []);
-
-  return (
-    <div>
-      <h1>This page is not available</h1>
-      <p>You are redirecting to google.com/about</p>
-    </div>
-  );
 
 
 export default MyApp
